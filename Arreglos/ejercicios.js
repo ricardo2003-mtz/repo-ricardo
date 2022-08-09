10221ea4626ebe42a3491dc93f2fc31eb01760f3
Ricardo <rpatio2003@gmail.com>
2022-08-08 20:21:14 -0500

    arreglos

---
+++ Arreglos/ejercicios.js
@@ -0,0 +1,83 @@
+/*let suma= 0
+let vector=[]
+let tam=Math.round((Math.random()*20-10)+10)
+console.log('valor de variables tam:',+tam)
+for (let i = 0; i <tam; i++) {
+   vector[i]=Math.round(Math.random()*100)
+    
+}
+console.log(vector)
+console.log('Tamaño de vector',+vector.length);
+
+for (let i = 0; i< vector.length; i++) {
+    suma=suma+vector[i]
+}
+console.log('La suma de todos los numeros es',+suma);*/
+
+
+/*let suma= 0
+let promedio=0
+let vector=[]
+let tam=Math.round((Math.random()*20-10)+10)
+console.log('valor de variables tam:',+tam)
+for (let i = 0; i <tam; i++) {
+   vector[i]=Math.round(Math.random()*100)
+    
+}
+console.log(vector)
+console.log('Tamaño de vector',+vector.length);
+
+for (let i = 0; i< vector.length; i++) {
+    suma=suma+vector[i]
+    promedio=suma/vector.length
+}
+console.log('La suma de todos los numeros es',+suma)
+console.log('El promedio de todos los numeros es',+promedio)*/
+
+/*let suma= 0
+let vector=[]
+let tam=Math.round((Math.random()*20-10)+10)
+console.log('valor de variables tam:',+tam)
+for (let i = 0; i <tam; i++) {
+   vector[i]=Math.round(Math.random()*100)
+    
+}
+console.log(mayor)
+console.log(vector)
+console.log('Tamaño de vector',+vector.length);
+*/
+
+/*let vector=[],
+    tamaño=Math.round((Math.random()*20-10)+10),
+    mayor=1
+    console.log('valor de variables tam:',+tamaño)
+for (let i = 0; i < tamaño; i++) {
+    vector[i]=Math.round(Math.random()*100)
+    if (vector[i]>mayor) {
+        mayor = vector[i]
+    }
+}
+console.log(vector)
+console.log('El numero menor de todos los anteriores es: '+mayor)
+
+let vector=[],
+    tamaño=Math.round((Math.random()*20-10)+10),
+    menor=100
+    console.log('valor de variables tam:',+tamaño)
+for (let i = 0; i < tamaño; i++) {
+    vector[i]=Math.round(Math.random()*100)
+    if (vector[i]<menor) {
+        menor = vector[i]
+    }
+}
+console.log(vector)
+console.log('El numero menor de todos los anteriores es: '+menor)*/
+
+let vector =[],
+tamaño =Math.round((Math.random()*20-10)+10)
+console.log('valor de las variables:'+tamaño)
+for (let i =i < tamaño; i++) {
+    vector[i]=Math.round(Math.random()*100)
+    
+}
+
