c27570fe30f8a4fe039ddbcf315cdf41bbfd88c8
ricardo2003-mtz <110370588+ricardo2003-mtz@users.noreply.github.com>
2022-09-18 20:03:06 -0500

    Update Clase del 29.js

--- Clase del 29.js
+++ Clase del 29.js
@@ -51,11 +51,6 @@ console.log(ob2.ficha);
 console.log(ob2)
 ob2.imprimirtodo();
 
-//---------------------------------------------------------------------------------------------------------
-
-//                                      Actividad 1
-
-
 class Publicacion{
     constructor(Titulo,Precio){
     this._Titulo=Titulo;
@@ -131,20 +126,6 @@ let ob4=new Libro ('Con el demonio dentro',200000,100);
 let ob5=new CD ('Funkint',150000,60,15000);
 ob5.TotalResultado();
 
-//                                 Parte 2
-
-/*
-Una agencia vendedora de autos desea un sistema computacional para administrar los datos de sus vehículos y clasificarlos por tipo. Todos los autos tienen los siguientes datos:
-· Número de serie del motor.
-· Marca.
-· Año.
-· Precio.
-Los vehículos se clasifican en autos compactos, autos de lujo, camionetas y vagonetas. Para los autos y vagonetas, también es importante almacenar la cantidad de pasajeros; mientras que para las camionetas se debe controlar la capacidad de carga en kgs. y la cantidad de ejes y de rodadas. Modele este sistema con diagramas de clases en UML e instancie cada una de las clases, asignándole datos mediante sus respectivas propiedades. Agregue un constructor con parámetros a cada clase para inicializar sus datos e invoque el constructor de la clase base desde el constructor de cada clase derivada (no utilice constructores default).
-Implemente la sobrescritura del método ToString() para mostrar los datos de cada tipo de auto.
-NOTA: No se permiten componentes duplicados en las clases ni clases vacías (sin elementos).
-*/
-
-/*--------------------------AUTOS ---superclase--------------------------*/
 class autos {
     constructor(nserie, marca, año, precio) {
             this._nserie = nserie;
@@ -191,7 +172,7 @@ class autos {
 let car = new autos(3245, 'Renauld', '2004', 50000)
 
 
-/*--------------------------AUTOS COMPACTOS---subclase--------------------------*/
+
 class autos_compactos extends autos {
     constructor(nserie, marca, año, precio, cantidad_de_pasajeros) {
             super(nserie, marca, año, precio)
@@ -218,21 +199,21 @@ class autos_compactos extends autos {
     set year(year) {
             year = this._año
         }
-        //Precio
+        
     get price() {
         return this._precio = this._precio;
     }
     set price(precio) {
             precio = this.precio
         }
-        //Cantidad de pasajeros
+        
     get cpa() {
         return this._cantidad_de_pasajeros = this._cantidad_de_pasajeros;
     }
     set cpa(cpa) {
             cpa = this._cantidad_de_pasajeros
         }
-        //Completo
+        
     completo() {
         console.log(this._nserie = this._nserie)
         console.log(this._marca = this._marca)
@@ -244,7 +225,7 @@ class autos_compactos extends autos {
 let vehiculo1 = new autos_compactos(14526, 'Cherolet', '2019', 60000000, 4)
 
 
-/*--------------------------AUTOS DE LUJO---subclase--------------------------*/
+
 
 class autos_lujo extends autos {
     constructor(nserie, marca, año, precio, cantidad_de_pasajeros) {
@@ -258,35 +239,35 @@ class autos_lujo extends autos {
     set nserie(noserie) {
             nserie = this._naserie
         }
-        //MARCA
+        
     get brand() {
         return this._marca = this._marca;
     }
     set brand(brand) {
             brand = this._marca
         }
-        //AÑO
+        
     get year() {
         return this._año = this._año;
     }
     set year(year) {
             year = this._año
         }
-        //Precio
+        
     get price() {
         return this._precio = this._precio;
     }
     set price(precio) {
             precio = this.precio
         }
-        //Cantidad de pasajeros
+        
     get cpa() {
         return this._cantidad_de_pasajeros = this._cantidad_de_pasajeros;
     }
     set cpa(cpa) {
             cpa = this._cantidad_de_pasajeros
         }
-        //Completo
+        
     completo() {
         console.log(this._nserie = this._nserie)
         console.log(this._marca = this._marca)
@@ -298,7 +279,7 @@ class autos_lujo extends autos {
 
 let vehicle1 = new autos_lujo(123456, 'LAamborjini', '2020', 100000000, 2)
 
-/*--------------------------CAMIONETAS---subclase--------------------------*/
+
 
 class camionetas extends autos {
     constructor(nserie, marca, año, precio, capacidad_carga, cantidad_ejes, cantidad_rodadas) {
@@ -307,57 +288,53 @@ class camionetas extends autos {
             this._cantidad_ejes = cantidad_ejes;
             this._cantidad_rodadas = cantidad_rodadas
         }
-        //N SERIE
-    get nserie() {
+            get nserie() {
         return this._nserie = this._nserie;
     }
     set nserie(noserie) {
             nserie = this._naserie
         }
-        //MARCA
+        
     get brand() {
         return this._marca = this._marca;
     }
     set brand(brand) {
             brand = this._marca
         }
-        //AÑO
+        
     get year() {
         return this._año = this._año;
     }
     set year(year) {
             year = this._año
         }
-        //Precio
+        
     get price() {
         return this._precio = this._precio;
     }
     set price(precio) {
             precio = this.precio
         }
-        //Capacidad de carga
+        
     get cc() {
         return this._capacidad_carga = this._capacidad_carga;
     }
     set cc(cc) {
             cc = this._capacidad_carga
         }
-        //Capacidad de ejes
-    get ce() {
+            get ce() {
         return this._capacidad_ejes = this._capacidad_ejes;
     }
     set ce(ce) {
             ce = this._capacidad_ejes
         }
-        //Capacidad de rodadas
-    get cr() {
+            get cr() {
         return this._cantidad_rodadas = this._cantidad_rodadas;
     }
     set cr(cr) {
             cr = this._cantidad_rodadas
         }
-        //Completo
-    completo() {
+           completo() {
         console.log(this._nserie = this._nserie)
         console.log(this._marca = this._marca)
         console.log(this._año = this._año)
@@ -370,50 +347,48 @@ class camionetas extends autos {
 
 let camioneta1 = new camionetas(2563214, 'Toyota', '2022', 80000000, 1400, 'Cuatro', 1000)
 
-/*--------------------------VAGONETAS---subclase--------------------------*/
+
 
 class vagoneta extends autos {
     constructor(nserie, marca, año, precio, cantidad_de_pasajeros) {
             super(nserie, marca, año, precio)
             this._cantidad_de_pasajeros = cantidad_de_pasajeros
         }
-        //N SERIE
+        
     get nserie() {
         return this._nserie = this._nserie;
     }
     set nserie(noserie) {
             nserie = this._naserie
         }
-        //MARCA
+        
     get brand() {
         return this._marca = this._marca;
     }
     set brand(brand) {
             brand = this._marca
         }
-        //AÑO
+        
     get year() {
         return this._año = this._año;
     }
     set year(year) {
             year = this._año
         }
-        //Precio
-    get price() {
+            get price() {
         return this._precio = this._precio;
     }
     set price(precio) {
             precio = this.precio
         }
-        //Cantidad de pasajeros
+        
     get cpa() {
         return this._cantidad_de_pasajeros = this._cantidad_de_pasajeros;
     }
     set cpa(cpa) {
             cpa = this._cantidad_de_pasajeros
         }
-        //Completo
-    completo() {
+            completo() {
         console.log(this._nserie = this._nserie)
         console.log(this._marca = this._marca)
         console.log(this._año = this._año)
@@ -423,18 +398,3 @@ class vagoneta extends autos {
 }
 
 let vagone = new vagoneta(152365, 'Volkswagen', '1995', 20000000, 8)
-
-
-/*---------------------------impresiones---------------------------
-console.log('_______________________________________________________________________')
-car.completo();
-console.log('_______________________________________________________________________')
-vehiculo1.completo();
-console.log('_______________________________________________________________________')
-vehicle1.completo();
-console.log('_______________________________________________________________________')
-camioneta1.completo();
-console.log('_______________________________________________________________________')
-vagone.completo();
-console.log('_______________________________________________________________________')
-*/
