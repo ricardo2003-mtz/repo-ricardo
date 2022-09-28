ef5abe5ec0c05e6b4bf4e5fcfe13909af5399c86
ricardo2003-mtz <110370588+ricardo2003-mtz@users.noreply.github.com>
2022-09-28 10:29:27 -0500

    Update quiz_clase.js

--- quiz_clase.js
+++ quiz_clase.js
@@ -1,11 +1,10 @@
-/*escriba una funcion que llene un array con n numerosn(n debe ser minimo 5 maximo 25)*/
+
 let arreglo = []
 for (let i = 0 ; i<20; i++) {
    arreglo [i]= (Math.round(Math.random()* 25-5)+5)
 }
 console.log(arreglo)
-/*escriba una funcion que reciba el array lleno y retorne otro arreglo con la diferencia que hay entre el valor de cada elemento 
-y la media del arreglo*/
+
 
 function media (arreglo){
     suma = 0
@@ -30,5 +29,3 @@ function diferencia(arreglo){
 console.log('la media del arreglo es:', ''+media(arreglo))
 console.log('la diferencia de cada valor es:','',+diferencia(arreglo))
 
-/*escriba una funcion que reciba el arreglo con las diferencias del punto anterior y diga de cada uno cuanto le falta o cuanto le sobra 
-segun el caso con referencia a la media*/
